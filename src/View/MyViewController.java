afa13187c6657a6d664add46ed7558210d223d06
Daniel Kolbasov <kolbasov@post.bgu.ac.il>
2020-06-21 20:59:04 +0300

    "update view controller"

--- src/View/MyViewController.java
+++ src/View/MyViewController.java
@@ -7,22 +7,23 @@ import javafx.fxml.FXML;
 import javafx.scene.input.ScrollEvent;
 import javafx.stage.Stage;
 
-public class MyViewController extends ScreenController{
-    @FXML private MazeDisplayer mazeDisplay;
+public class MyViewController extends ScreenController {
+    @FXML
+    private MazeDisplayer mazeDisplay;
 
     @FXML
-    public void initialize(){
-        mazeDisplay.setWidth(main.getWidth()-50);
-        mazeDisplay.setHeight(main.getHeight()-100);
+    public void initialize() {
+        mazeDisplay.setWidth(main.getWidth() - 50);
+        mazeDisplay.setHeight(main.getHeight() - 100);
         mazeDisplay.drawMaze(viewModel.getMaze());
 
         main.heightProperty().addListener(new ChangeListener<Number>() {
             @Override
             public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
 
-//                mazeDisplay.setHeight(main.getHeight()-100);
-                mazeDisplay.setScaleY(mazeDisplay.getScaleY() * (1 + (newValue.intValue() - oldValue.intValue()) / (float) oldValue.intValue()));
-//                mazeDisplay.draw();
+                mazeDisplay.setHeight(main.getHeight() - 100);
+//                mazeDisplay.setScaleY(mazeDisplay.getScaleY() * (1 + (newValue.intValue() - oldValue.intValue()) / (float) oldValue.intValue()));
+                mazeDisplay.draw();
 
             }
         });
@@ -30,16 +31,16 @@ public class MyViewController extends ScreenController{
             @Override
             public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
 
-//                mazeDisplay.setWidth(main.getWidth()-50);
-                mazeDisplay.setScaleX(mazeDisplay.getScaleX() * (1 + (newValue.intValue() - oldValue.intValue()) / (float) oldValue.intValue()));
-//                mazeDisplay.draw();
+                mazeDisplay.setWidth(main.getWidth() - 50);
+//                mazeDisplay.setScaleX(mazeDisplay.getScaleX() * (1 + (newValue.intValue() - oldValue.intValue()) / (float) oldValue.intValue()));
+                mazeDisplay.draw();
             }
         });
         mazeDisplay.setOnScroll((ScrollEvent event) -> {
             // Adjust the zoom factor as per your requirement
             double zoomFactor = 1.05;
             double deltaY = event.getDeltaY();
-            if (deltaY < 0){
+            if (deltaY < 0) {
                 zoomFactor = 2.0 - zoomFactor;
             }
             mazeDisplay.setScaleX(mazeDisplay.getScaleX() * zoomFactor);
@@ -48,7 +49,7 @@ public class MyViewController extends ScreenController{
     }
 
     @FXML
-    public void showSolution(){
+    public void showSolution() {
 
     }
 }
